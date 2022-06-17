b3b917b24812838a22944df8adff9a9331e44468
Kevin Bell <bellKevin@pm.me>
2022-06-17 15:03:01 +0000

    Update MuseumStore.java

--- museumStore/src/museumstore/MuseumStore.java
+++ museumStore/src/museumstore/MuseumStore.java
@@ -2,10 +2,12 @@ package museumstore;
 
 class MuseumStore {
 
+    //variables
     private String item, category;
     private double price;
     private int quantity;
 
+    //do the work
     public MuseumStore(String item, String category, double price, int quantity) {
         this.item = item;
         this.category = category;
@@ -15,10 +17,8 @@ class MuseumStore {
 
     public void sell(int quantity) {
         System.out.printf("\nSell %d of %s\n", quantity, this.item);
-        if (quantity <= this.quantity)
-        {
-            System.out.printf("Sold %d %s, cost $%.2f\n",
-                    quantity, this.item, this.price * quantity);
+        if (quantity <= this.quantity) {
+            System.out.printf("Sold %d %s, cost $%.2f\n", quantity, this.item, this.price * quantity);
             this.quantity -= quantity;
         } else {
             System.out.printf("No sale, not enough %s in stock\n", this.item);
@@ -27,7 +27,6 @@ class MuseumStore {
     }
 
     public void display() {
-        System.out.printf("Item: %s, category %s, price = $%.2f, quantity = %d\n",
-                this.item, this.category, this.price, this.quantity);
+        System.out.printf("Item: %s, category %s, price = $%.2f, quantity = %d\n", this.item, this.category, this.price, this.quantity);
     }
 }
